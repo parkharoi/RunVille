30640006f75fd28ce656b25b878bd68c34613569
바카로이 <haeun8520@naver.com>
2026-04-16 21:30:47 +0900

    supabase 연결 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:run_ville/app/app.dart';
 import 'package:run_ville/core/config/env.dart';
 
+// main.dart의 방향성
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Env.initialize();
+
+  await Supabase.initialize(url: Env.supabaseUrl, anonKey: Env.supabaseAnonKey);
 
   runApp(const ProviderScope(child: RunVilleApp()));
 }
